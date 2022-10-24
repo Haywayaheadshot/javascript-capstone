31dfc784c72a18b9878e4e62f4a40d4ea4e3b48a
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-24 22:18:07 +0000

    Create new image const and append to logo-div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+import pageLogo from './images/icons/page-logo.png';
+
+// create new image?
+const imageDiv = document.querySelector('.logo-div');
+const logo = new Image();
+logo.src = pageLogo;
+logo.alt = 'Movie Logo';
+logo.className = 'Logo';
+imageDiv.appendChild(logo);
