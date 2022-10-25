dc05fba75258eaf58dc563ec51f5c7e27fe267a4
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-25 13:00:54 +0000

    Move code to create card into CreateCard module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 import pageLogo from './images/icons/page-logo.png';
-import likeMovie from './images/icons/like-movie.png';
+import createCard from './modules/CreateCard';
 
 // create new image?
 const imageDiv = document.querySelector('.logo-div');
@@ -11,24 +11,4 @@ logo.alt = 'Movie Logo';
 logo.className = 'Logo';
 imageDiv.appendChild(logo);
 
-// Create card image
-const cardContainer = document.querySelector('.cards-container');
-const cardDiv = document.createElement('div');
-cardDiv.className = "card";
-cardDiv.innerHTML = 
-`
-<img src="${pageLogo}" alt="Card Logo" class="card-image" />
-<section class="card-title">
-  <h2 class="font card-header-text">Card One</h2>
-  <img src="${likeMovie}" class="like-movie" alt="Love Card" />
-</section>
-<div class="like-count font">Like count</div>
-<button class="comments-button font">
-  Comments
-</button>
-<button class="reservations-button font">
-Reservations
-</button>
-`
-
-cardContainer.appendChild(cardDiv);
+createCard();
