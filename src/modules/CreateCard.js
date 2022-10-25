db505a633c16487d8c555b00bf4bc7067f59c2f1
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-25 14:46:14 +0000

    Eslint fix spacing error

--- src/modules/CreateCard.js
+++ src/modules/CreateCard.js
@@ -1,16 +1,13 @@
 import likeMovie from '../images/icons/like-movie.png';
 
 const createCard = async () => {
-    await fetch('https://api.tvmaze.com/shows').then((response) => response.json()).then((completeResponse) => {
-        console.log(completeResponse)
-
-       for (let i = 0; i < 20; i++) {
-           // Create card image
-           const cardContainer = document.querySelector('.cards-container');
-           const cardDiv = document.createElement('div');
-           cardDiv.className = "card";
-           cardDiv.innerHTML = 
-           `
+  await fetch('https://api.tvmaze.com/shows').then((response) => response.json()).then((completeResponse) => {
+    for (let i = 0; i < 20; i += 1) {
+      // Create card image
+      const cardContainer = document.querySelector('.cards-container');
+      const cardDiv = document.createElement('div');
+      cardDiv.className = 'card';
+      cardDiv.innerHTML = `
            <img src="${completeResponse[i].image.medium}" alt="Card Logo" class="card-image" />
            <section class="card-title">
              <h2 class="font card-header-text">${completeResponse[i].name}</h2>
@@ -23,14 +20,11 @@ const createCard = async () => {
            <button class="reservations-button font">
            Reservations
            </button>
-           `
-           
-           cardContainer.appendChild(cardDiv);
+           `;
 
-       }
-
-    })
-}
+      cardContainer.appendChild(cardDiv);
+    }
+  });
+};
 
 export default createCard;
-
