2194d9b3f21ce7179e74c914bcad9a1894c1c542
taldr27 <diegogarciaq98@gmail.com>
2022-10-25 14:39:06 -0500

    Fix linter errors

--- src/modules/btnListener.js
+++ src/modules/btnListener.js
@@ -2,7 +2,7 @@ import change from './change.js';
 
 export default function btnListener(ar) {
   const commentBtn = document.getElementById(`btn-${ar}`);
-  const body = document.querySelector('body');
+  // const body = document.querySelector('body');
   commentBtn.addEventListener('click', () => {
     const popupContainer = document.getElementById('popup-container');
     if (popupContainer.style.display === '') {
