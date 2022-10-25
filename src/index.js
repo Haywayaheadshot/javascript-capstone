41097c04f6d1e0bd8b68b44c6357226f7f5be2e8
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-25 14:44:31 +0000

    Add .js extension to createCard import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 import pageLogo from './images/icons/page-logo.png';
-import createCard from './modules/CreateCard';
+import createCard from './modules/CreateCard.js';
 
 // create new image?
 const imageDiv = document.querySelector('.logo-div');
