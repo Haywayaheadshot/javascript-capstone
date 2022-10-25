0212e14191e721f6d7b2fe793f7e03e3e1316370
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-25 14:09:08 +0000

    fetch data from tvmaze api

--- src/modules/CreateCard.js
+++ src/modules/CreateCard.js
@@ -1,28 +1,35 @@
 import likeMovie from '../images/icons/like-movie.png';
-import pageLogo from '../images/icons/page-logo.png';
 
-const createCard = () => {
-    // Create card image
-    const cardContainer = document.querySelector('.cards-container');
-    const cardDiv = document.createElement('div');
-    cardDiv.className = "card";
-    cardDiv.innerHTML = 
-    `
-    <img src="${pageLogo}" alt="Card Logo" class="card-image" />
-    <section class="card-title">
-      <h2 class="font card-header-text">Card One</h2>
-      <img src="${likeMovie}" class="like-movie" alt="Love Card" />
-    </section>
-    <div class="like-count font">Like count</div>
-    <button class="comments-button font">
-      Comments
-    </button>
-    <button class="reservations-button font">
-    Reservations
-    </button>
-    `
-    
-    cardContainer.appendChild(cardDiv);
+const createCard = async () => {
+    await fetch('https://api.tvmaze.com/shows').then((response) => response.json()).then((completeResponse) => {
+        console.log(completeResponse)
+
+       for (let i = 0; i < 20; i++) {
+           // Create card image
+           const cardContainer = document.querySelector('.cards-container');
+           const cardDiv = document.createElement('div');
+           cardDiv.className = "card";
+           cardDiv.innerHTML = 
+           `
+           <img src="${completeResponse[i].image.medium}" alt="Card Logo" class="card-image" />
+           <section class="card-title">
+             <h2 class="font card-header-text">${completeResponse[i].name}</h2>
+             <img src="${likeMovie}" class="like-movie" alt="Love Card" />
+           </section>
+           <div class="like-count font">Like count</div>
+           <button class="comments-button font">
+             Comments
+           </button>
+           <button class="reservations-button font">
+           Reservations
+           </button>
+           `
+           
+           cardContainer.appendChild(cardDiv);
+
+       }
+
+    })
 }
 
 export default createCard;
