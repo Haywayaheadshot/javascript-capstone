a6f7d32881653da1210225e2b2991ee1561310cb
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-26 14:56:24 +0000

    Remove stray console.log

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,7 +1,6 @@
 const here = async (ar) => {
   const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/g56oldW5hMhMsaYERIvY/comments?item_id=${ar}`);
   const json = await response.json();
-  console.log(json);
   const commentContainer = document.querySelector('#comments');
   commentContainer.innerHTML = '';
   json.forEach((tv) => {
