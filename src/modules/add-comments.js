d55002faf33a3b2c93473ed7a207392ddb372499
taldr27 <diegogarciaq98@gmail.com>
2022-10-26 12:47:50 -0500

    Refactor function addcomment

--- src/modules/add-comments.js
+++ src/modules/add-comments.js
@@ -4,17 +4,18 @@ const addComment = async () => {
   const nameValue = document.getElementById('nameComment').value;
   const commentValue = document.getElementById('descriptionComment').value;
   const idBtn = parseInt(document.querySelector('.btnAdd').id, 10);
-  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/g56oldW5hMhMsaYERIvY/comments?item_id=${idBtn}`, {
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LH7R1NNqzZ0S67pGVqTQ/comments?item_id=${idBtn}`, {
     method: 'POST',
     body: JSON.stringify({ item_id: idBtn, username: nameValue, comment: commentValue }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  }).then((response) => console.log('comment' + response.status));
+  });
+  //   .then((response) => console.log('comment' + response.status));
   //   e.target.reset();
   //   document.querySelector('#input-name').value = '';
   //   document.querySelector('#input-score').value = '';
-  console.log(idBtn);
+  //   console.log(idBtn);
   here(idBtn);
 };
 
