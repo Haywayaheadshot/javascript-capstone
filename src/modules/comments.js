908dd969c1bfa65d03f439c364402505ae9ebf7a
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2022-10-27 20:50:41 +0000

    Update code to empty input values once submitted

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,20 +1,28 @@
 const here = async (ar) => {
   const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LH7R1NNqzZ0S67pGVqTQ/comments?item_id=${ar}`);
-  const json = await response.json();
-  console.log(json.length);
-  const commentContainer = document.querySelector('#comments');
+  const commentsBox = document.getElementById('comments');
   const commentTitle = document.getElementById('commentTitle');
-  if (json.length === undefined) {
+  if (response.status === 400) {
+    commentsBox.innerHTML = 'No comments yet';
     commentTitle.innerHTML = 'Comments (0)';
   } else {
-    commentTitle.innerHTML = `Comments (${json.length})`;
+    const json = await response.json();
+    const commentContainer = document.querySelector('#comments');
+    if (json.length === undefined) {
+      commentTitle.innerHTML = 'Comments (0)';
+    } else {
+      commentTitle.innerHTML = `Comments (${json.length})`;
+    }
+    commentContainer.innerHTML = '';
+    const nameValue = document.getElementById('nameComment');
+    const commentValue = document.getElementById('descriptionComment');
+    nameValue.value = '';
+    commentValue.value = '';
+    json.forEach((tv) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `<h3>${tv.creation_date} ${tv.username}:  ${tv.comment}</h3>`;
+      commentContainer.appendChild(row);
+    });
   }
-  commentContainer.innerHTML = '';
-  json.forEach((tv) => {
-    const row = document.createElement('tr');
-    row.innerHTML = `<h3>${tv.creation_date} ${tv.username}:  ${tv.comment}</h3>`;
-    commentContainer.appendChild(row);
-  });
 };
-
 export default here;
