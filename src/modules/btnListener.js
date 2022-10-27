88a0570cf96c322ade535272855b661cf415c280
taldr27 <diegogarciaq98@gmail.com>
2022-10-27 10:20:20 -0500

    Add final function to show the comments inside of the popup

--- src/modules/btnListener.js
+++ src/modules/btnListener.js
@@ -5,15 +5,16 @@ import here from './comments.js';
 export default function btnListener(ar) {
   const commentBtn = document.getElementById(`btn-${ar}`);
   // const body = document.querySelector('body');
-  commentBtn.addEventListener('click', () => {
+  commentBtn.addEventListener('click', async () => {
     const popupContainer = document.getElementById('popup-container');
     if (popupContainer.style.display === '') {
       popupContainer.style.display = 'flex';
     }
     const submitBtn = document.querySelector('.btnAdd');
+
     submitBtn.id = `${ar}`;
     submitBtn.addEventListener('click', addComment);
     change(ar);
-    here(ar);
+    await here(ar);
   });
 }
--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,7 +1,14 @@
 const here = async (ar) => {
   const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LH7R1NNqzZ0S67pGVqTQ/comments?item_id=${ar}`);
   const json = await response.json();
+  console.log(json.length);
   const commentContainer = document.querySelector('#comments');
+  const commentTitle = document.getElementById('commentTitle');
+  if (json.length === undefined) {
+    commentTitle.innerHTML = 'Comments (0)';
+  } else {
+    commentTitle.innerHTML = `Comments (${json.length})`;
+  }
   commentContainer.innerHTML = '';
   json.forEach((tv) => {
     const row = document.createElement('tr');
